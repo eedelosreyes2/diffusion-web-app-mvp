3d6ee037a6a13912bd1d4631546d139faa585d49
Elijah Delos Reyes <elijah@Elijahs-MBP.lan>
2021-05-19 15:48:34 -0700

    drag and drop + google login

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React, { Component } from "react";
+import "./app.css";
+import ReactImage from "./react.png";
+import DragAndDropComponent from "./components/DragAndDropComponent";
 
 export default class App extends Component {
-  state = { username: null };
+  state = { isLoggedIn: false, username: null, lists: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    if (this.isLoggedIn()) {
+      this.setState({ isLoggedIn: true });
+      this.fetchData();
+    } else {
+      this.setState({ isLoggedIn: false });
+    }
   }
+
+  isLoggedIn = () => {
+    // fetch user logged in info from browser data
+
+    // if logged in -> set username, return true
+
+    // if not logged in -> return false
+
+    return true;
+  };
+
+  fetchData = () => {
+    fetch("/api/getUsername")
+      .then((res) => res.json())
+      .then((user) => this.setState({ username: user.username }));
+    fetch("/api/getLists")
+      .then((res) => res.json())
+      .then((user) => this.setState({ username: user.username }));
+  };
 
   render() {
     const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        {/* {username ? (
+          <h1>{`Hello ${username}`}</h1>
+        ) : (
+          <h1>Loading.. please wait!</h1>
+        )}
+        */}
+        {this.state.isLoggedIn ? <DragAndDropComponent /> : ""}
       </div>
     );
   }
---
+++ src/client/components/LogInComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class LogInComponent extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default LogInComponent;
