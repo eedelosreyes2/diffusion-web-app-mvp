dc4a4b05c8629ef0a06fba4fe466d7a67144b01e
Elijah Delos Reyes <elijah@Elijahs-MBP.lan>
2021-05-26 19:11:28 -0700

    logout button

--- src/client/components/LogOutComponent.js
+++ src/client/components/LogOutComponent.js
@@ -3,14 +3,7 @@ import GoogleLogout from "react-google-login";
 
 export class LogOutComponent extends Component {
   render() {
-    return (
-      <GoogleLogout
-        clientId="173875502237-vqno633dqovkrmnot06va4r1iu0m2882.apps.googleusercontent.com"
-        buttonText="Logout"
-        onSuccess={this.props.responseGoogleLogout}
-        onFailure={this.props.responseGoogleLogout}
-      />
-    );
+    return <button onClick={this.props.responseGoogleLogout}>Logout</button>;
   }
 }
 
