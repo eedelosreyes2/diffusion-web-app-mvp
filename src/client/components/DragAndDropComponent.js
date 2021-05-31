30f6fb69ed486222856217128b8ba58d818eb503
Elijah Delos Reyes <elijah@Elijahs-MBP.lan>
2021-05-30 23:10:02 -0700

    so close

--- src/client/components/DragAndDropComponent.js
+++ src/client/components/DragAndDropComponent.js
@@ -110,7 +110,40 @@ function DragAndDropComponent(props) {
                   }}
                   {...provided.droppableProps}
                 >
-                  I am a droppable!
+                  {content.map(
+                    ({ contentId, url, contentTitle, brainfart, category }) => {
+                      return (
+                        <Draggable
+                          key={contentId}
+                          draggableId={contentTitle}
+                          index={contentId}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                  userSelect: "none",
+                                  padding: 16,
+                                  margin: "0 0 8px 0",
+                                  minHeight: "50px",
+                                  backgroundColor: snapshot.isDragging
+                                    ? "#263B4A"
+                                    : "#456C86",
+                                  color: "white",
+                                  ...provided.draggableProps.style,
+                                }}
+                              >
+                                {contentTitle}
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      );
+                    }
+                  )}
                   {provided.placeholder}
                 </div>
               )}
