6a1abea392c2a69bdb379f9be17d7002fbbb6d79
Elijah Delos Reyes <delosreyes.elijah@gmail.com>
2021-06-29 20:41:13 -0700

    remove firebase hosting

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const app = express();
-
-app.get("/timestamp", (req, res) => {
-    res.send(`${Date.now()}`)
-})
-
-exports.app = functions.https.onRequest(app);
